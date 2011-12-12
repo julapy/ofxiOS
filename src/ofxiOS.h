61df40685258df62596caa672a747183ce26eca6
lukasz.karluk <julapy@googlemail.com>
2011-12-12 11:41:50 +1100

    first commit

---
+++ src/ofxiOS.h
@@ -0,0 +1,11 @@
+//
+//  ofxiOS.h
+//  emptyExample
+//
+//  Created by lukasz karluk on 9/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#pragma once
+
+#import "ofxiOSWindow.h"
