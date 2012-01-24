2709897dcc6c3f26829e03e28a996c97b8fd8c19
lukasz.karluk <julapy@googlemail.com>
2012-01-24 16:54:54 +1100

    added ofxiOSUtils for texture reloading.

---
+++ src/ofxiOSUtils.h
@@ -0,0 +1,19 @@
+//
+//  ofxiOSUtils.h
+//  emptyExample
+//
+//  Created by lukasz karluk on 23/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#pragma once
+
+void ofPauseVideoGrabbers();
+void ofResumeVideoGrabbers();
+
+void ofReloadAllImageTextures();
+
+void ofUnloadAllFontTextures();
+void ofReloadAllFontTextures();
+
+void ofUpdateBitmapCharacterTexture();
